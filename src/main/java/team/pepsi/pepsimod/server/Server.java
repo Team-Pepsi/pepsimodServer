1c7233e2bc21806fee009a1851c56623d4a87f0f
DaPorkchop_ <joey.rabil@gmail.com>
2017-09-02 17:27:49 +0200

    fix up to work with pepsimodcommons

--- src/main/java/team/pepsi/pepsimod/server/Server.java
+++ src/main/java/team/pepsi/pepsimod/server/Server.java
@@ -1,6 +1,6 @@
 package team.pepsi.pepsimod.server;
 
-import team.pepsi.pepsimod.common.ClientAuthInfo;
+import team.pepsi.pepsimod.common.*;
 
 import java.io.*;
 import java.net.ServerSocket;
@@ -10,46 +10,133 @@ import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
 public class Server {
-    public static HashMap<String, byte[]> pepsimod = new HashMap<String, byte[]>();
+    public static HashMap<String, byte[]> pepsimod = new HashMap<>();
+    public static HashMap<String, byte[]> assets = new HashMap<>();
     public static Timer timer = new Timer();
+    public static DataTag tag = new DataTag(new File(DataTag.HOME_FOLDER.getPath() + DataTag.HOME_FOLDER.separatorChar + ".pepsimodaccounts.dat"));
 
     public static void main(String[] args) {
-        try {
-            populateArray(args[0]); //path to pepsimod jar
-            ServerSocket socket = new ServerSocket(48273);
-            while (true) {
-                System.out.println("[Main thread] Waiting for client...");
-                final Socket clientSocket = socket.accept();
-                System.out.println("[Main thread] Accepted client " + clientSocket.getInetAddress() + ":" + clientSocket.getPort() + ", passing to processing thread");
-                schedule(() -> {
-                    try {
-                        ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
-                        ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
-                        Object obj = in.readObject();
-                        ClientAuthInfo info;
-                        if (obj instanceof ClientAuthInfo)  {
-                            info = (ClientAuthInfo) obj;
-                        } else {
-                            System.out.println("OBJ WAS NOT INSTANCE OF CLIENTAUTHINFO!");
-                            System.out.println(obj.getClass().getCanonicalName());
-                            return;
-                        }
-                        //TODO: do some auth
-                        //TODO: encrypt classes before sending
-                        out.writeObject(pepsimod);
-                        out.flush();
-
-                        out.close();
-                        in.close();
-                        clientSocket.close();
-                    } catch (IOException | ClassNotFoundException e) {
-                        e.printStackTrace();
+        populateArray(args[0]); //path to pepsimod jar
+        new Thread() {
+            public void run() {
+                try {
+                    ServerSocket socket = new ServerSocket(48273);
+                    while (true) {
+                        System.out.println("[Main thread] Waiting for client...");
+                        final Socket clientSocket = socket.accept();
+                        System.out.println("[Main thread] Accepted client " + clientSocket.getInetAddress() + ":" + clientSocket.getPort() + ", passing to processing thread");
+                        new Thread() {
+                            public void run() {
+                                ObjectOutputStream out = null;
+                                ObjectInputStream in = null;
+                                try {
+                                    out = new ObjectOutputStream(clientSocket.getOutputStream());
+                                    in = new ObjectInputStream(clientSocket.getInputStream());
+                                    Object obj = in.readObject();
+                                    ClientAuthInfo info;
+                                    if (obj instanceof ClientAuthInfo) {
+                                        info = (ClientAuthInfo) obj;
+                                    } else {
+                                        System.out.println("OBJ WAS NOT INSTANCE OF CLIENTAUTHINFO!");
+                                        System.out.println(obj.getClass().getCanonicalName());
+                                        throw new ClassNotFoundException("Excpected ClientAuthInfo");
+                                    }
+                                    User user = (User) tag.getSerializable(info.username);
+                                    if (user == null)   {
+                                        out.writeObject(new ServerLoginErrorMessage(0));
+                                        throw new IllegalStateException("No such user!");
+                                    } else {
+                                        if (user.password.equals(info.password))    {
+                                            //user verified!
+                                            if (user.hwid == null)  {
+                                                //need to get HWID for encryption
+                                                out.writeObject(new ServerFirstAuthMessage());
+                                                out.flush();
+                                                obj = in.readObject();
+                                                ClientHWIDSendMessage hwidSendMessage;
+                                                if (obj instanceof ClientHWIDSendMessage) {
+                                                    hwidSendMessage = (ClientHWIDSendMessage) obj;
+                                                } else {
+                                                    System.out.println("OBJ WAS NOT INSTANCE OF CLIENTHWIDSENDMESSAGE!");
+                                                    System.out.println(obj.getClass().getCanonicalName());
+                                                    throw new ClassNotFoundException("Excpected ClientHWIDSendMessage");
+                                                }
+                                                user.hwid = hwidSendMessage.hwid;
+                                            }
+                                        } else {
+                                            out.writeObject(new ServerLoginErrorMessage(1));
+                                            out.flush();
+                                            throw new IllegalStateException("INVALID CREDENTIALS");
+                                        }
+                                    }
+                                    switch (info.nextRequest)   {
+                                        case 0: //play
+                                            out.writeObject(new ServerPepsiModSending(pepsimod, assets, user.hwid));
+                                            out.flush();
+                                            break;
+                                        case 1: //change password
+                                            ClientChangePassword changePassword;
+                                            if (obj instanceof ClientChangePassword) {
+                                                changePassword = (ClientChangePassword) obj;
+                                            } else {
+                                                System.out.println("OBJ WAS NOT INSTANCE OF CLIENTCHANGEPASSWORD!");
+                                                System.out.println(obj.getClass().getCanonicalName());
+                                                throw new ClassNotFoundException("Excpected ClientChangePassword");
+                                            }
+                                            user.password = changePassword.newPassword;
+                                    }
+                                } catch (IOException | ClassNotFoundException | IllegalStateException e) {
+                                    e.printStackTrace();
+                                } finally {
+                                    try {
+                                        if (out != null) {
+                                            out.close();
+                                        }
+                                        if (in != null) {
+                                            in.close();
+                                        }
+                                        clientSocket.close();
+                                    } catch (IOException e) {
+                                        //WTF JAVA
+                                    }
+                                }
+                            }
+                        }.start();
                     }
-                }, 10);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-            //WTF JAVA!
+        }.start();
+        schedule(() -> {
+            for (Iterator<Map.Entry<String, Serializable>> iterator = tag.objs.entrySet().iterator(); iterator.hasNext();)   {
+                if (iterator.next().getValue() == null) {
+                    iterator.remove();
+                }
+            }
+            tag.save();
+        }, 60 * 60 * 1000);
+        Scanner scanner = new Scanner(System.in);
+        while (true)    {
+            String input = scanner.nextLine().trim();
+            String[] split = input.split(" ");
+            switch (split[0])   {
+                case "add":
+                    User user = new User();
+                    user.username = split[1];
+                    user.password = split[2];
+                    tag.setSerializable(user.username, user);
+                    break;
+                case "remove":
+                    tag.setSerializable(split[1], null);
+                    break;
+                case "resethwid":
+                    ((User) tag.getSerializable(split[1])).hwid = null;
+                    break;
+                case "save":
+                    tag.save();
+                    break;
+            }
         }
     }
 
---
+++ src/main/java/team/pepsi/pepsimod/server/User.java
@@ -0,0 +1,9 @@
+package team.pepsi.pepsimod.server;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    public String username = null;
+    public String password = null;
+    public String hwid = null;
+}
